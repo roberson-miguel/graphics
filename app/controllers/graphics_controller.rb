fe9ae820514c1ebec123ff3e46745a1fdaffc41b
Roberson Miguel <roberson.miguel@biosbug.com.br>
2020-09-07 16:08:31 -0300

    Refatora Where na indes em  graphics

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -6,21 +6,25 @@ class GraphicsController < ApplicationController
     @courses = Course.all
 
    
-    @administracao = Course.where(title: :Administração).group_by
-    @financas = Course.where(title: :Finanças).group_by
-    @contabilidade = Course.where(title: :Contabilidade).group_by
-    @informatica = Course.where(title: :Informática).group_by
-    @bancodedados = Course.where(title: :"Banco de Dados").group_by
-    @filosofia = Course.where(title: :Filosofia).group_by
-    @geografia = Course.where(title: :Geografia).group_by
-    @direito = Course.where(title: :Contabilidade).group_by
-    @pedagogia = Course.where(title: :Pedagogia).group_by
-    @enfermagem = Course.where(title: :Enfermagem).group_by
-    @engenharia = Course.where(title: :Engenharia).group_by
-    @educacaofisica = Course.where(title: :"Educação Física").group_by
-    @arquitetura = Course.where(title: :Arquitetura).group_by
-    @hotelaria = Course.where(title: :Hotelaria).group_by
-  
+    #@administracao = Course.where(title: :Administração).group_by
+
+
+    @administracao = Course.where("title = 'Administração' AND shift_id = 1", params[:orders], false)
+    
+    @financas = Course.where("title = 'Finanças' AND shift_id = 1", params[:orders], false)
+    @contabilidade = Course.where("title = 'Contabilidade' AND shift_id = 1", params[:orders], false)
+    @informatica = Course.where("title = 'Informática' AND shift_id = 1", params[:orders], false)
+    @bancodedados = Course.where("title = 'Banco de Dados' AND shift_id = 1", params[:orders], false)
+    @filosofia = Course.where("title = 'Filosofia' AND shift_id = 1", params[:orders], false)
+    @geografia = Course.where("title = 'Geografia' AND shift_id = 1", params[:orders], false)
+    @direito = Course.where("title = 'Direito' AND shift_id = 1", params[:orders], false)
+    @pedagogia = Course.where("title = 'Pedagogia' AND shift_id = 1", params[:orders], false)
+    @enfermagem = Course.where("title = 'Enfermagem' AND shift_id = 1", params[:orders], false)
+    @engenharia = Course.where("title = 'Engenharia' AND shift_id = 1", params[:orders], false)
+    @educacaofisica = Course.where("title = 'Educação Física' AND shift_id = 1", params[:orders], false)
+    @arquitetura = Course.where("title = 'Arquitetura' AND shift_id = 1", params[:orders], false)
+    @hotelaria = Course.where("title = 'Hotelaria' AND shift_id = 1", params[:orders], false)
+
     
    
   end
